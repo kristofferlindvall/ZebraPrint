24310ab8bc19ca3de055e88961f8ee1a07e76727
Kristoffer Lindvall <kristoffer@distancify.com>
2018-05-15 22:34:17 +0200

    Initial commit

---
+++ ZebraPrint/ZebraPrint/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿// Samples found here: http://techdocs.zebra.com/link-os/2-13/android/content/com/zebra/sdk/comm/ConnectionBuilder.html
+
+using LinkOS.Plugin;
+using LinkOS.Plugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ZebraPrint
+{
+	public partial class MainPage : ContentPage
+	{
+        private List<IDiscoveredPrinter> _bluetoothPrinters = new List<IDiscoveredPrinter>();
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+
+        }
+        
+        private MainViewModel ViewModel { get { return BindingContext as MainViewModel; } }
+
+        private void SendBluetooth(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ViewModel.SelectedBluetoothPrinter == null)
+                {
+                    DisplayAlert("Error", "Select bluetooth device first", "Ok");
+                    return;
+                }
+
+                
+
+                // Instantiate a Bluetooth connection
+                var conn = ConnectionBuilder.Current.Build("BT:" + ViewModel.SelectedBluetoothPrinter.Address);
+                // Alternative:
+                //var conn = ViewModel.SelectedBluetoothPrinter.Connection;
+
+                // Open the connection - physical connection is established here.
+                conn.Open();
+                
+                //Set printer to ZPL mode
+                WriteString(conn, "! U1 setvar \"device.languages\" \"zpl\"\r\n");
+
+                WriteString(conn, "^XA^FO20,20^A0N,25,25^FDHello World!^FS^XZ");
+                
+                // Close the connection to release resources.
+                conn.Close();
+
+            }
+            catch (Zebra.Sdk.Comm.ConnectionException ex)
+            {
+                // Handle communications error here.
+                Debug.WriteLine(ex.StackTrace);
+            }
+
+            //try
+            //{
+
+            //    // Instantiate TCP connection
+            //    Connection thePrinterConn = ConnectionBuilder.build("BT:" + theIpAddress + ":9100");
+
+            //    // Open the connection - physical connection is established here.
+            //    thePrinterConn.open();
+
+            //    // This example prints "This is a ZPL test." near the top of the label.
+            //    String zplData = "^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ";
+
+            //    // Send the data to printer as a byte array.
+            //    thePrinterConn.write(zplData.getBytes());
+
+            //    // Close the connection to release resources.
+            //    thePrinterConn.close();
+
+            //}
+            //catch (ConnectionException e)
+            //{
+
+            //    // Handle communications error here.
+            //    e.printStackTrace();
+            //}
+        }
+
+        private void WriteString(IConnection connection, string data)
+        {
+            var bytes = Encoding.UTF8.GetBytes(data);
+            connection.Write(bytes);
+        }
+    }
+
+}
---
+++ ZebraPrint/ZebraPrint/MainViewModel.cs
@@ -0,0 +1,77 @@
+﻿using LinkOS.Plugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ZebraPrint
+{
+    public class MainViewModel : INotifyPropertyChanged, IDiscoveryHandler
+    {
+        public MainViewModel()
+        {
+            Task.Run(() =>
+            {
+                LinkOS.Plugin.BluetoothDiscoverer.Current.FindPrinters(Forms.Context, this);
+            });
+        }
+
+        private List<IDiscoveredPrinterBluetooth> _bluetoothPrinters = new List<IDiscoveredPrinterBluetooth>();
+
+        public List<IDiscoveredPrinterBluetooth> BluetoothPrinters
+        {
+            get
+            {
+                return _bluetoothPrinters.ToList();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void FoundPrinter(IDiscoveredPrinter discoveredPrinter)
+        {
+            if (discoveredPrinter is IDiscoveredPrinterBluetooth bt)
+            {
+                _bluetoothPrinters.Add(bt);
+                OnPropertyChanged(nameof(BluetoothPrinters));
+                Debug.WriteLine("Bluetooth device found");
+            }
+        }
+
+        public void DiscoveryFinished()
+        {
+            Debug.WriteLine("Bluetooth device discovery finished");
+        }
+
+        public void DiscoveryError(string message)
+        {
+            Debug.WriteLine(message);
+        }
+
+        private IDiscoveredPrinterBluetooth _selectedBluetoothPrinter;
+        public IDiscoveredPrinterBluetooth SelectedBluetoothPrinter
+        {
+            get
+            {
+                return _selectedBluetoothPrinter;
+            }
+            set
+            {
+                if (_selectedBluetoothPrinter != value)
+                {
+                    _selectedBluetoothPrinter = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+    }
+}
